fa85970052b20d05457b626bd5ab804e2b9a3979
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-04-22 20:30:18 +0300

    Add files via upload

--- maze.py
+++ maze.py
@@ -3,10 +3,14 @@ from pygame import *
 init()
 mixer.init()
 mixer.music.load('Cypis_-_Polskaya_Korova.mp3')
-mixer.music.play()
+#mixer.music.play()
 mixer.music.set_volume(0.3)
 
-WIDTH, HEIGHT = 1100,700
+
+MAP_WIDTH, MAP_HEIGHT = 25, 20
+TILESIZE = 30
+
+WIDTH, HEIGHT = MAP_WIDTH*TILESIZE, MAP_HEIGHT*TILESIZE
 window = display.set_mode((WIDTH, HEIGHT))
 FPS = 90
 clock = time.Clock()
@@ -48,7 +52,36 @@ class Player(NPC):
             self.rect.x -= self.speed
         
         
-player = Player(p_img, 70, 70, 300, 300)
+class Bos(NPC):
+    def __init__(self, sprite_img, width, height, x, y):
+        super().__init__(sprite_img, width, height, x, y)
+        self.damage = 10
+        self.speed = 1
+
+    # def run(self):
+    #     if key_pressed[K_w] and self.rect.y > 0 and self.rect.bottom < HEIGHT and self.rect.right < WIDTH and self.rect.x > 0:
+    #         self.rect.y -= self.speed
+
+player = Player(p_img, 30, 30, 300, 300)
+#warrior = Bos(warrior_img, 100, 70, 500, 500)
+walls = sprite.Group()
+with open("map.txt", "r") as f:
+    map = f.readlines()
+    x = 0
+    y = 0
+    for line in map:
+        for symbol in line:
+            if symbol == "w":#стіни
+                walls.add(NPC(wall_img, TILESIZE, TILESIZE, x, y ))
+            if symbol == "p":#гравець
+                player.rect.x = x
+                player.rect.y = y
+            if symbol == "t":
+                treasure = NPC(treasure_img, TILESIZE, TILESIZE, x, y)
+            x += TILESIZE
+        y += TILESIZE
+        x = 0
+
 
 run = True
 while run:
