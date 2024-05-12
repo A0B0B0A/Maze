de89e114c61d7a8d93780a892a4ad033ab0a675e
A0B0B0A <152081588+A0B0B0A@users.noreply.github.com>
2024-05-12 17:26:07 +0300

    Add files via upload

--- maze.py
+++ maze.py
@@ -7,6 +7,10 @@ mixer.music.load('Cypis_-_Polskaya_Korova.mp3')
 mixer.music.play()
 mixer.music.set_volume(0.3)
 
+font.init()
+
+font1 = font.SysFont('Impact', 100)
+game_over_text = font1.render("Game Over!", True, (255, 0, 0))
 
 MAP_WIDTH, MAP_HEIGHT = 25, 20
 TILESIZE = 30
@@ -56,9 +60,12 @@ class Player(NPC):
         collide_list = sprite.spritecollide(self, walls, False, sprite.collide_mask)
         if len(collide_list) > 0:
             self.rect.x, self.rect.y = old_pos
+        
+        bos_hit = sprite.spritecollide(self, boses, False, sprite.collide_mask)
+        if len(bos_hit):
+            self.hp -= 100
 
-        
-        
+     
 class Bos(NPC):
     def __init__(self, sprite_img, width, height, x, y):
         super().__init__(sprite_img, width, height, x, y)
@@ -77,9 +84,12 @@ class Bos(NPC):
             self.rect.y -= self.speed
         elif self.dir == 'down':
             self.rect.y += self.speed
+        
         collide_list = sprite.spritecollide(self, walls, False, sprite.collide_mask)
         if len(collide_list) > 0:
             self.rect.x, self.rect.y = old_pos
+            self.dir = random.choice(self.dir_list)
+            
 
 player = Player(p_img, 20, 20, 300, 300)
 walls = sprite.Group()
@@ -103,15 +113,28 @@ with open("map.txt", "r") as f:
         y += TILESIZE
         x = 0
 
-
+finish = False
 run = True
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     window.blit(bg,(0,0))
-
+    
+    if player.hp <= 0 :
+        finish = True
+    
+    if sprite.collide_mask(player, treasure):
+        finich = True
+        game_over_text = font1.render("You win!", True, (0, 255, 0))
+        
     all_sprites.draw(window)
-    all_sprites.update()
+
+    if not finish:
+        all_sprites.update()
+  
+    if finish:
+        window.blit(game_over_text, (170, 230))
+
     display.update()
     clock.tick(FPS)
